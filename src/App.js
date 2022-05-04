33bb978b3b4672b2399c48008b97f13ed626bd67
Sanjeet <ssj256x@outlook.com>
2022-05-04 13:26:57 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This change was made on the feature branch
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
